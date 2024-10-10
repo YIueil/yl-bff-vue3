66e13f652ea7a7dd2688fa19ba197a0b6756ea13
弋孓 <YIueil@163.com>
2024-10-10 16:08:40 +0800

    feat(ant design vue): 初始化 # 实现自定义样式和暗黑模式

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,7 @@
-<script setup lang="ts">
-import ComponentsView from '@/views/ComponentsView.vue'
-import { useApplicationStore } from '@/stores/application'
-import { storeToRefs } from 'pinia'
-
-const applicationStore = useApplicationStore()
-const { darkMode } = storeToRefs(applicationStore)
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div
-    :style="{height: '100vh', color: !darkMode ? '#FFFFFFFF' : '#181818FF', background: !darkMode ? '#FFFFFFFF' : '#181818FF'}">
-    <ComponentsView />
-  </div>
+  <router-view />
 </template>
 
 <style scoped>
--- src/stores/application.ts
+++ src/stores/application.ts
@@ -1,8 +1,18 @@
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useApplicationStore = defineStore('application', () => {
   const darkMode = ref(false)
 
-  return { darkMode }
+  //region 主题样式变更
+  const setDarkMode = (mode: boolean) => {
+    darkMode.value = mode
+  }
+  const classList = document.documentElement.classList
+  const setClass = (dark: boolean) => classList[dark ? 'add' : 'remove']('dark')
+  watch(darkMode, newValue => {
+    setClass(newValue)
+  })
+  //endregion
+  return { darkMode, setDarkMode }
 })
